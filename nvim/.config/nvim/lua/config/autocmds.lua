5d769f1baba875100ef12f6823fb79ec9f2718e2
Yeonwoo Kim <kywoopid@gmail.com>
2025-01-13 21:52:08 +0900

    Disable spellcheck in markdown file

--- nvim/.config/nvim/lua/config/autocmds.lua
+++ nvim/.config/nvim/lua/config/autocmds.lua
@@ -6,3 +6,13 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+--
+
+-- https://github.com/LazyVim/LazyVim/discussions/4021
+-- auto spelling 기능을 꺼서, *.md 파일 스펠링 문법 기능 끈다
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "markdown",
+  callback = function()
+    vim.opt_local.spell = false
+  end,
+})
